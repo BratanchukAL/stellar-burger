6dcfa2bb199d0249883ee114d27d96be371f047d
Bratanchuk Alexey (4076727) <bratanchuk@dev.vtb.ru>
2023-08-23 09:38:42 +0300

    [add] BurgerConstructorPage: layout for BurgerIngredients, BurgerConstructor

--- src/components/pages/burger-constructor-page/burger-constructor-page.tsx
+++ src/components/pages/burger-constructor-page/burger-constructor-page.tsx
@@ -1,13 +1,21 @@
+import React from "react";
+
 import {BurgerIngredients} from "components/widgets/burger-ingredients";
 import {BurgerConstructor} from "components/widgets/burger-constructor";
-import React from "react";
+
+import styles from './burger-constructor-page.module.css'
 
 
 export const BurgerConstructorPage = () => {
     return(
-        <>
-            <BurgerIngredients/>
-            <BurgerConstructor/>
-        </>
+        <section className={styles.page}>
+            <p className={styles.title}>
+                Соберите бургер
+            </p>
+            <div className={styles.ingredients_and_constructor}>
+                <BurgerIngredients/>
+                <BurgerConstructor/>
+            </div>
+        </section>
     )
 }
