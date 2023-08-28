77b5af3869b684b1400304205f3a290e36738a32
Bratanchuk Alexey (4076727) <bratanchuk@dev.vtb.ru>
2023-08-28 20:06:37 +0300

    [add] modal

--- src/components/shared/ui/modal/modal.tsx
+++ src/components/shared/ui/modal/modal.tsx
@@ -1,57 +1,62 @@
 import React, {FC, KeyboardEvent, useEffect, useRef} from "react";
 import ReactDOM from "react-dom";
 
+import {CloseIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import {clx} from "components/shared/utils";
+
 import styles from './modal.module.css'
 
 
 const modalRoot = document.getElementById("react-modals")!;
 const bodyElement = document.getElementsByTagName("body")[0]!;
 
-interface ModalProps{
 
+interface ModalProps{
+    onClose: (() => void)
 }
 
-export const Modal: FC<React.PropsWithChildren<ModalProps>> = ({children, ...props}) => {
+export const Modal: FC<React.PropsWithChildren<ModalProps>> = ({
+    onClose,
+    children, ...props
+}) => {
+
     const bodyRef = useRef(bodyElement)
 
-    useEffect(()=>{
+    useEffect(() => {
         bodyRef.current.style.overflow = 'hidden'
 
-        return ()=> {
+        return () => {
             bodyRef.current.style.overflow = ''
         }
-    },[bodyRef])
+    }, [bodyRef])
 
     const handleCloseByKeyDown = (e:KeyboardEvent<HTMLInputElement>) =>{
         if (e.code === 'Escape')
-            bodyRef.current.style.overflow = ''
+            onClose()
+    }
+
+    const handleClose = () =>{
+        onClose()
     }
 
     return ReactDOM.createPortal(
-            (
-                <>
-                    <div  className={styles.overlay} > </div>
-                    <div className={styles.modal} onKeyDown={handleCloseByKeyDown} tabIndex={-1}>
-                        <div className={styles.modal_dialog} >
+        (
+            <>
+                <div className={styles.overlay}> </div>
+                <div className={styles.modal} onKeyDown={handleCloseByKeyDown} tabIndex={-1}>
+                    <div className={clx(styles.modal_dialog, ['p-10'])}>
+                        <div className={styles.close_icon_content}>
+                            <CloseIcon type="primary" onClick={handleClose}/>
+                        </div>
+                        <div className={styles.modal_content}>
                             hello
                             {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs hello
-                            {children}
-                            fdsfs
                         </div>
                     </div>
-                </>
-            ),
-            modalRoot
-        );
-}
+                </div>
+            </>
+        ),
+        modalRoot
+    );
+};
