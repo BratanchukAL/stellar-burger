773adc2b6d2e6625bfac6265bc2f16b282136f91
Bratanchuk Alexey (4076727) <bratanchuk@dev.vtb.ru>
2023-09-09 13:10:27 +0300

    [add] RTK Query for order

--- src/components/shared/api/index.ts
+++ src/components/shared/api/index.ts
@@ -1,8 +1,8 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
+import {createApi, fetchBaseQuery, retry} from "@reduxjs/toolkit/dist/query/react";
 import {API_URL} from "../configs/api";
 
 export const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: API_URL}),
+    baseQuery:  retry(fetchBaseQuery({baseUrl: API_URL}), {maxRetries:10}),
     endpoints: builder => ({})
 })
--- src/components/shared/configs/api.ts
+++ src/components/shared/configs/api.ts
@@ -1,2 +1,3 @@
 export const API_URL = 'https://norma.nomoreparties.space/api/'
 export const API_PATH_INGREDIENTS = 'ingredients'
+export const API_PATH_ORDER = 'orders'
