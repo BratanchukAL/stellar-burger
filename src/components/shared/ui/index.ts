65b83c163ea76add08c0c8cd13f4c8e79454c2be
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-25 18:22:48 +0300

    [add] ErrorText,

--- src/components/shared/ui/index.ts
+++ src/components/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export { ButtonLink } from "./button-link/button-link";
 export {EmailInput} from "./email-input/email-input";
+export {ErrorText} from "./error-text/error-text";
 export { Modal } from "./modal/modal";
