dceb2dee6dcb5451dc5309496ffa07938aa3c366
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-26 19:41:43 +0300

    [add] widgets: ResetForm

--- src/components/widgets/auth/reset-form/reset-form.tsx
+++ src/components/widgets/auth/reset-form/reset-form.tsx
@@ -1,57 +1,72 @@
-import React, {ChangeEvent} from 'react'
-
+import React from 'react'
+import {Navigate, useLocation} from "react-router-dom";
 import {Button, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components"
 
-import {ButtonLink} from "components/shared/ui";
+import {RoutesPath} from "components/shared/configs";
+import {ErrorText, WarningText} from "components/shared/ui";
+import {useForm} from "components/shared/hooks";
+
+import {useHandleReset} from "components/features/auth/reset";
+
+import {TextNavLink} from "../ui/text-nav-link";
+import {Footer} from "../ui/footer";
 
 import styles from './reset-form.module.css'
 
 
 export const ResetForm = ()=>{
-    const [value, setValue] = React.useState('')
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value)
-    }
+    const {state: stateLocation} = useLocation()
+    const [state, onChange] = useForm({
+        password: "",
+        token: "",
+    })
+    const [onReset, response] = useHandleReset(state)
+
     return(
         <>
-            <PasswordInput
-                placeholder={'Введите новый пароль'}
-                onChange={onChange}
-                value={value}
-                name={'new_password'}
-                extraClass="mt-6"
-            />
-            <Input
-              type={'text'}
-              placeholder={'Введите код из письма'}
-              onChange={onChange}
+            {response.isLoading && <div>Loading...</div>}
+            {stateLocation?.sentEmail ||
+                <Navigate to={RoutesPath.forgot} replace/>
+            }
+            <WarningText message={response.data?.message} extraClass="mt-6"/>
+            <ErrorText message={response.error?.data?.message} extraClass="mt-6"/>
+            <form onSubmit={onReset} className={styles.container_form}>
 
-              value={value}
-              name={'code_email'}
-              error={false}
-              // ref={inputRef}
+                <PasswordInput
+                    placeholder={'Введите новый пароль'}
+                    onChange={onChange}
+                    value={state.password}
+                    name={'password'}
+                    extraClass="mt-6"
+                    required
+                />
+                <Input
+                  type={'text'}
+                  placeholder={'Введите код из письма'}
+                  onChange={onChange}
 
-              errorText={'Ошибка'}
-              size={'default'}
-              extraClass="mt-6"
-            />
-            <Button
-                htmlType="button"
-                type="primary"
-                size="medium"
-                extraClass={styles.submit + " mt-6"}
-            >
-                Сохранить
-            </Button>
+                  value={state.token}
+                  name={'token'}
+                  extraClass="mt-6"
+                  required
+                />
+                <Button
+                    htmlType="submit"
+                    type="primary"
+                    size="medium"
+                    extraClass="mt-6"
+                >
+                    Сохранить
+                </Button>
+            </form>
 
-            <div className={styles.footer + " mt-20"}>
-                <p className="text text_type_main-default text_color_inactive mt-4">
-                    Вспомнили пароль?
-                    <ButtonLink  type="secondary" size="large" to="/login">
-                        Войти
-                    </ButtonLink>
-                </p>
-            </div>
+            <Footer>
+                <TextNavLink
+                    pretext={'Вспомнили пароль?'}
+                    to={RoutesPath.login}
+                    text={'Войти'}
+                />
+            </Footer>
         </>
     )
 }
