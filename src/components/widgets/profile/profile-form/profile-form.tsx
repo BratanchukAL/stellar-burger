20f2241074d060947aeb2d35e040ce1618825ed9
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-27 22:21:04 +0300

    [upd] ProfileForm: visibleEditButtons, refetchProfile

--- src/components/widgets/profile/profile-form/profile-form.tsx
+++ src/components/widgets/profile/profile-form/profile-form.tsx
@@ -18,6 +18,8 @@ import styles from './profile-form.module.css'
 
 export const ProfileForm = ()=> {
     const [toggleResetForm, setResetForm] = useState(false)
+    const [visibleEditButtons, setVisibleEditButtons] = useState(false)
+
     const {data: profile, isLoading, error, refetch: refetchProfile} = useGetProfileQuery()
 
     const [state, onChange] = useForm(
@@ -31,20 +33,32 @@ export const ProfileForm = ()=> {
 
     const [updateProfile, responseUpdate] = useUpdateProfile(state)
 
-    useEffect(()=>{
-        if (!responseUpdate.isLoading)
-            refetchProfile()
-    }, [responseUpdate])
+    const handleSubmit = async (e: any) => {
+        await updateProfile(e)
+        refetchProfile()
+        setVisibleEditButtons(false)
+    }
+
+    const handleChange = (e: any) => {
+        onChange(e)
+        if(!visibleEditButtons)
+            setVisibleEditButtons(true)
+    }
+
+    const handleCancel = ()=>{
+        setResetForm(!toggleResetForm)
+        setVisibleEditButtons(false)
+    }
 
     return (
         <>
             {(isLoading || responseUpdate.isLoading) && <div>Loading...</div>}
             <ErrorText message={(error as any)?.data?.message} extraClass="mt-6 mb-6"/>
             <ErrorText message={responseUpdate.error?.data?.message} extraClass="mb-6"/>
-            <form onSubmit={updateProfile} className={styles.container_form}>
+            <form onSubmit={handleSubmit} className={styles.container_form}>
                 <EditInput
                     placeholder={'Имя'}
-                    onChange={onChange}
+                    onChange={handleChange}
                     value={state.name}
                     name={'name'}
                     isIcon={true}
@@ -52,7 +66,7 @@ export const ProfileForm = ()=> {
                 />
                 <EmailInput
                     placeholder={'E-mail'}
-                    onChange={onChange}
+                    onChange={handleChange}
                     value={state.email}
                     name={'email'}
                     isIcon={true}
@@ -61,16 +75,16 @@ export const ProfileForm = ()=> {
                 />
                 <PasswordInput
                     placeholder={'Пароль'}
-                    onChange={onChange}
+                    onChange={handleChange}
                     value={state.password}
                     name={'password'}
                     icon="EditIcon"
                     extraClass="mt-6"
                     required
                 />
-                <div className={styles.buttons}>
+                {visibleEditButtons && <div className={styles.buttons}>
                     <Button
-                        onClick={()=>setResetForm(!toggleResetForm)}
+                        onClick={handleCancel}
                         htmlType="button"
                         type="secondary"
                         size="medium"
@@ -86,7 +100,7 @@ export const ProfileForm = ()=> {
                     >
                         Сохранить
                     </Button>
-                </div>
+                </div>}
             </form>
         </>
     );
