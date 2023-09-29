db54ac26f00a9f3e1e718926b21529ffdfa83805
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-29 17:58:56 +0300

    [upd] SpinnerWidget: text of refreshTokenThunk

--- src/components/features/auth/refresh-token/thunk.ts
+++ src/components/features/auth/refresh-token/thunk.ts
@@ -3,6 +3,7 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {RootStateType} from "components/providers/store";
 
 import {ITokens, selectRefreshToken, sessionActions} from "components/entities/session";
+import {selectText, spinnerActions} from "components/entities/spinner";
 
 import {RefreshTokenBody} from "./api/types";
 import {refreshTokenAPI} from "./api/api";
@@ -18,7 +19,10 @@ export const refreshTokenThunk = createAsyncThunk<
     async (_,  api) => {
         const dispatch = api.dispatch
         const refreshToken = selectRefreshToken(api.getState())
+        const bufferTextSpinner = selectText(api.getState())
+
         dispatch(sessionActions.loading())
+        dispatch(spinnerActions.start("Идет проверка токена. Подождите..."))
 
          // send refresh token to get new access token
         const body:RefreshTokenBody ={
@@ -34,6 +38,7 @@ export const refreshTokenThunk = createAsyncThunk<
         if (response.isSuccess && refreshResult) {
             // store the new token
             dispatch(sessionActions.refresh(refreshResult))
+            dispatch(spinnerActions.start(bufferTextSpinner))
         } else {
             dispatch(sessionActions.logout())
         }
