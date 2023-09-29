ab2dce365837c66a7877012d1576f59046268247
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-29 10:41:32 +0300

    [add] ingredientDetails: persist

---
+++ src/components/entities/products/ingredient-details/persist.ts
@@ -0,0 +1,15 @@
+import storage from "redux-persist/lib/storage";
+import {persistReducer} from "redux-persist";
+
+import {slice} from "./slice";
+
+export const persistConfig = {
+    key: slice.name,
+    storage,
+    // blacklist:['error','isLoading']
+}
+
+// Persisted reducer
+slice.reducer = persistReducer(persistConfig, slice.reducer) as unknown as typeof slice.reducer
+
+export const IngredientDetailsSlice = slice;
