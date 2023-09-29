d8940d51e02c00dd3e8002579fe47946df7975fe
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-09-29 15:27:58 +0300

    [add] spinner

--- src/components/shared/ui/spinner/spinner.tsx
+++ src/components/shared/ui/spinner/spinner.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Overlay} from "../overlay/overlay";
 
+import styles from './spinner.module.css'
+
 export const Spinner = () => {
     return (
-        <>
-            <Overlay />
-            <div className="lds-spinner"> </div>
-        </>
+        <div className={styles.content}>
+            {/*<Overlay />*/}
+            <div className={styles.lds_spinner}>
+                <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+            </div>
+        </div>
     )
 }
