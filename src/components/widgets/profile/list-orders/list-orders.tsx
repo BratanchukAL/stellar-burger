1bed8917644824804f7e33b71fb4615b1002f163
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-10-22 10:11:50 +0300

    [add] order: OrderCard(sample), ListOrders (sample)

--- src/components/widgets/profile/list-orders/list-orders.tsx
+++ src/components/widgets/profile/list-orders/list-orders.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 import {OrderCard} from "components/entities/order/ui/order-card";
 
+import styles from './list-orders.module.css'
+
 
 
 export const ListOrders = () => {
     return(
-        <>
-            Здесь лист заказов
+        <section className={styles.box}>
             <OrderCard />
-        </>
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
+        </section>
     )
 }
