e09ad8d275945071eea119c0700c8d9a335ccbeb
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-10-27 12:25:21 +0300

    [add] typing: TypedActionsFromEntities

--- src/components/entities/index.ts
+++ src/components/entities/index.ts
@@ -1,11 +1,22 @@
+import {ReturnSliceActionsType} from "components/shared/utils";
+
 import {basketSlice} from "./basket/slice";
-import {reducersProducts} from "./products";
+import {reducersProducts, TypedActionsFromProducts} from "./products";
 import {sessionSlice} from "./session";
 import {spinnerSlice} from "./spinner";
 
+
 export const reducersEntities = {
     [spinnerSlice.name]: spinnerSlice.reducer,
-    [basketSlice.name]: basketSlice.reducer,
+    [basketSlice.name]:  basketSlice.reducer,
     [sessionSlice.name]: sessionSlice.reducer,
     ...reducersProducts,
 }
+
+
+//typing actions
+export type TypedActionsFromEntities =
+    TypedActionsFromProducts |
+    ReturnSliceActionsType<typeof spinnerSlice.actions> |
+    ReturnSliceActionsType<typeof basketSlice.actions> |
+    ReturnSliceActionsType<typeof sessionSlice.actions>
--- src/components/entities/products/index.ts
+++ src/components/entities/products/index.ts
@@ -1,8 +1,17 @@
+import {ReturnSliceActionsType} from "components/shared/utils";
+
 import {IngredientDetailsSlice} from "./ingredient-details/persist";
 
 export { useGetProductsQuery } from "./api/api";
 export type { IProduct } from "./models";
 
+
+
 export const reducersProducts = {
     [IngredientDetailsSlice.name]: IngredientDetailsSlice.reducer
 }
+
+
+//typing actions
+export type TypedActionsFromProducts =
+    ReturnSliceActionsType<typeof IngredientDetailsSlice.actions>
