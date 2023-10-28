d782929c88678fa64dad877977c515ecbf8daeb5
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-10-28 15:05:14 +0300

    [add] websocketMiddlewareBase

---
+++ src/components/shared/middlewares/websocket-middleware-base.ts
@@ -0,0 +1,73 @@
+import type {AnyAction, Middleware, MiddlewareAPI} from 'redux';
+
+import {ActionCreatorWithPayload, AsyncThunk, PayloadAction, ThunkDispatch} from "@reduxjs/toolkit";
+
+
+
+export const websocketMiddlewareBase =
+    <
+        AppDispatch extends ThunkDispatch<RootState, unknown, AnyAction>,
+        RootState,
+        AppActions extends PayloadAction
+    >
+    (
+        baseUrl: string,
+        path: string,
+
+        wsStartAction:  ActionCreatorWithPayload<void, any>,
+        wsDisconnectAction:  ActionCreatorWithPayload<void, any>,
+
+        onSuccessAction:  ActionCreatorWithPayload<Event, any> | AsyncThunk<void, Event, any>,
+        onErrorAction:  ActionCreatorWithPayload<Event, any> | AsyncThunk<void, Event, any>,
+        onMessageAction:  ActionCreatorWithPayload<Event, any> | AsyncThunk<void, Event, any>,
+        onClosedAction:  ActionCreatorWithPayload<CloseEvent, any> | AsyncThunk<void, CloseEvent, any>,
+    ): Middleware => {
+
+    return ((store: MiddlewareAPI<AppDispatch, RootState>) => {
+        let socket: WebSocket | null = null;
+        // let closing: boolean = false
+
+        return next => (action: PayloadAction) => {
+            const {dispatch, getState} = store;
+            const {type, payload} = action;
+
+            if (wsStartAction.match(action)) {
+                // объект класса WebSocket
+                socket = new WebSocket(baseUrl+path);
+            }
+
+            if (socket) {
+                // функция, которая вызывается при открытии сокета
+                socket.onopen = (event: Event) => {
+                    dispatch(onSuccessAction(event));
+                };
+
+                // функция, которая вызывается при ошибке соединения
+                socket.onerror = (event: Event) => {
+                    dispatch(onErrorAction(event));
+                };
+
+                // функция, которая вызывается при получения события от сервера
+                socket.onmessage = (event:  MessageEvent) => {
+                    const {data} = event; //JSON.load(data)
+                    dispatch(onMessageAction(event));
+                };
+                // функция, которая вызывается при закрытии соединения
+                socket.onclose = (event: CloseEvent) => {
+                    if (event.code === 0 && event.reason === 'closing')
+                        dispatch(onClosedAction(event));
+                    else
+                        dispatch(wsStartAction())
+                };
+
+                if (wsDisconnectAction.match(action)) {
+                    // closing = true
+                    socket.close(0, 'closing')
+                    socket = null
+                }
+            }
+
+            next(action);
+        };
+    }) as Middleware;
+}; 
