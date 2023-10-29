d008bb30f3ce4eae5747634637e85b8ace775ba6
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-10-29 14:42:52 +0300

    [upd] order-card: StatusDictionary, childrenCalcPrice

--- src/components/entities/order/ui/order-card/order-card.tsx
+++ src/components/entities/order/ui/order-card/order-card.tsx
@@ -4,20 +4,28 @@ import {FormattedDate} from "@ya.praktikum/react-developer-burger-ui-components"
 
 import {clx} from "components/shared/utils";
 
-
 import {IOrder} from "../../models";
 
 import styles from './order-card.module.css'
 
 
 
-interface IOrderCardProps {
-    order: IOrder
-    childrenComposition: React.ReactNode
+const StatusDictionary = {
+    done: 'Готово',
+    created: 'Создан',
+    pending: 'Готовится',
+    cancelled: 'Отменен'
 }
 
 
-export const OrderCard:FC<IOrderCardProps> = ({order, childrenComposition}) => {
+interface IOrderCardProps {
+    order: IOrder
+    childrenComposition: React.ReactNode
+    childrenCalcPrice: React.ReactNode
+}
+
+
+export const OrderCard:FC<IOrderCardProps> = ({order, childrenComposition, childrenCalcPrice}) => {
     const date = new Date(order.createdAt)
 
 
@@ -31,10 +39,11 @@ export const OrderCard:FC<IOrderCardProps> = ({order, childrenComposition}) => {
                 <p className="text text_type_main-medium">{order.name}</p>
             </div>
             <div className={clx(styles.item_info, ['pt-2'])}>
-                <p className="text text_type_main-default">{order.status}</p>
+                <p className="text text_type_main-default">{StatusDictionary[order.status]}</p>
             </div>
             <div className={clx(styles.item_info, ['pt-6'])}>
                 {childrenComposition}
+                {childrenCalcPrice}
             </div>
         </div>
     )
