756d4277d0eed2684bff648de686f089c43d84d2
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-10-29 20:54:20 +0300

    [add] ListOrders in profile

--- src/components/widgets/profile/list-orders/list-orders.tsx
+++ src/components/widgets/profile/list-orders/list-orders.tsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
+import {useAppDispatch, useAppSelector} from "components/providers/store";
+
+import {ErrorText} from "components/shared/ui";
+
+import {
+    ordersAllOfUserWSDisconnectAction,
+    ordersAllOfUserWSStartAction,
+} from "components/entities/order";
 import {OrderCard} from "components/entities/order/ui/order-card";
+import {selectOrdersAllOfUser} from "components/entities/order";
+
+import {useGetProductsQuery} from "components/entities/products";
+import {ProductsShortComposition} from "components/entities/products/ui/products-short-composition";
 
 import styles from './list-orders.module.css'
+import {CalcPrice} from "components/features/calc-price";
+
 
 
 
 export const ListOrders = () => {
-    return(
-        <section className={styles.box}>
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-        </section>
+    const dispatch = useAppDispatch()
+    const {isStreaming, data: {orders}, isError:isErrorWs, error:errorWs} = useAppSelector(selectOrdersAllOfUser)
+
+
+    const {
+        data: products = [],
+        isError,
+        error
+    } = useGetProductsQuery()
+
+    useEffect(()=>{
+
+        !isStreaming && !isErrorWs && dispatch(ordersAllOfUserWSStartAction())
+
+        return ()=>{
+            isStreaming && dispatch(ordersAllOfUserWSDisconnectAction())
+        }
+    }, [dispatch, isStreaming, isErrorWs])
+
+
+    return(<>
+        {
+            isErrorWs ? <ErrorText message={errorWs} extraClass="mt-6"/> :
+            isError ? <ErrorText message={(error as any)?.error} extraClass="mt-6"/> :
+            products &&
+                <section className={styles.box}>
+                    {orders && orders.map((v) => {
+                        return (
+                            <OrderCard key={v.number} order={v}
+                                childrenComposition={
+                                    <ProductsShortComposition ids={v.ingredients} products={products}/>
+                                }
+                               childrenCalcPrice={<CalcPrice ids={v.ingredients} products={products}/>}
+                            />
+                        )
+                    })}
+                </section>
+        }
+        </>
     )
 }
