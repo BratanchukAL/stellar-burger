078ad7dee3e1ec50f8c18cc336a5f6d1d97a41ad
Bratanchuk Alexey <alexeybratanchuk@cps.ai>
2023-11-02 01:25:27 +0300

    [upd] OrderCompositionPage

--- src/components/pages/order-composition-page/order-composition-page.tsx
+++ src/components/pages/order-composition-page/order-composition-page.tsx
@@ -1,9 +1,12 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from "react-router-dom";
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
+import {useNavigate, useParams, useNavigationType} from "react-router-dom";
 
-import {useAppDispatch} from 'components/providers/store';
+import {useAppDispatch, useAppSelector} from 'components/providers/store';
 
-import {IOrder, orderAPI} from "components/entities/order";
+import {ROUTES} from "components/shared/configs";
+import {Modal} from "components/shared/ui";
+
+import {IOrder, orderAPI, selectOrdersAll, selectOrdersAllOfUser} from "components/entities/order";
 
 import {OrderCompositionWidget} from "components/widgets/order-composition-widget";
 
@@ -13,21 +16,59 @@ import styles from './order-composition-page.module.css'
 
 
 export const OrderCompositionPage = () =>{
-    const id_param = useParams().id
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+    const transitionType = useNavigationType()
+    const id_param = useParams().id
+
     const [order, setOrder] = useState<IOrder>({} as IOrder)
 
+    const {isStreaming, data: {orders}} = useAppSelector(selectOrdersAll)
+    const {isStreaming:isStreamingOfUser, data: {orders: ordersOfUser}} = useAppSelector(selectOrdersAllOfUser)
+
     useEffect(()=>{
-        dispatch(orderAPI.endpoints.getOrder.initiate({number: Number(id_param)}))
-            .then((response)=>setOrder(response.data as IOrder))
+        if (!isStreaming || !isStreamingOfUser)
+            dispatch(orderAPI.endpoints.getOrder.initiate({number: Number(id_param)}))
+                .then((response)=>setOrder(response.data as IOrder))
     }, [dispatch, id_param])
+
+
+    const currentOrder: IOrder | undefined = useMemo<IOrder>(()=>{
+        let r = {} as IOrder | undefined
+        if (isStreaming)
+            r = orders.find((v)=> v.number === Number(id_param))
+        else if (isStreamingOfUser)
+            r = ordersOfUser.find((v)=> v.number === Number(id_param))
+        else if (order?.ingredients)
+            r = order
+
+        return r as IOrder
+    },
+    [id_param, orders, ordersOfUser, isStreaming, isStreamingOfUser, order])
+
+
+    const handleClose = useCallback(()=>{
+            navigate(ROUTES.ORDERS_IN_PROFILE)
+    }, [navigate])
 
 
     return(
         <>
-            <section className={styles.page}>
-                <OrderCompositionWidget order={order}/>
-            </section>
+            {!currentOrder?.ingredients && <div>Не найден такой заказ</div>}
+            {currentOrder?.ingredients && (transitionType !== 'POP') &&
+
+                <Modal onClose={handleClose}>
+                    <div className={styles.content_modal}>
+                        <OrderCompositionWidget order={currentOrder}/>
+                    </div>
+                </Modal>
+            }
+            {currentOrder?.ingredients && (transitionType === 'POP') &&
+
+                <section className={styles.page}>
+                    <OrderCompositionWidget order={currentOrder}/>
+                </section>
+            }
         </>
     )
 }
